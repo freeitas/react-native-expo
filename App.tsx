ed04e0f047f0f2b9b8ebbef71095654badb100a5
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-08 22:08:29 -0300

    chore: added theme

--- App.tsx
+++ App.tsx
@@ -2,19 +2,21 @@ import { Text, View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme';
+
 import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
-       <StatusBar 
+    <NativeBaseProvider theme={THEME}>
+       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
