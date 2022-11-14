e29bd2c1c7584a36ea3c5e094d7326db9e35a65d
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-14 11:13:18 -0300

    feat: added Home header component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Text, VStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { UserPhoto } from './UserPhoto';
+
+import userDefault from '@assets/userPhotoDefault.png';
+
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={userDefault}
+        size={16}
+        alt="User image"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.200" fontSize="md">
+          Hello,
+        </Text>
+
+        <Heading color="gray.100" fontSize="md">
+          User
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
