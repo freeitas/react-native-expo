4b629e2997a95849fc3ce5a0a8bae273a5e82239
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-14 11:28:09 -0300

    feat: added Home Group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import { FlatList, VStack, HStack } from 'native-base';
+
 import { HomeHeader } from '@components/HomeHeader';
-import { Text, VStack } from 'native-base';
+import { Group } from '@components/Group';
 
 export function Home() {
+  const [groups, setGroups] = useState(['Back', 'Biceps', 'Triceps', 'Shoulder'])
+  const [groupSelected, setGroupSelected] = useState('Back')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   );
 }
