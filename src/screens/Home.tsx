7e7d559463b026316530503af6f0c1d2f7dc70b7
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-14 16:16:28 -0300

    feat: navigation exercise details button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, VStack, HStack, Heading, Text } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
 
 import { ExerciseCard } from '@components/ExerciseCard';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 export function Home() {
   const [exercises, setExercises] = useState(['Front Pull', 'Bent Row', 'One-sided row', 'Deadlift']);
   const [groups, setGroups] = useState(['Back', 'Biceps', 'Triceps', 'Shoulder'])
   const [groupSelected, setGroupSelected] = useState('Back')
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -49,7 +57,7 @@ export function Home() {
           data={exercises}
           keyExtractor={item => item}
           renderItem={({ item }) => (
-            <ExerciseCard name={item} />
+            <ExerciseCard name={item} onPress={handleOpenExerciseDetails}/>
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,87 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading} from 'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+
+import userDefault from '@assets/userPhotoDefault.png';
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Profile' />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={6} px={10}>
+        {
+            photoIsLoading ?
+              <Skeleton 
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded="full"
+                startColor="gray.500"
+                endColor="gray.400"
+              />
+            :
+              <UserPhoto 
+                source={userDefault}
+                alt="User photo"
+                size={PHOTO_SIZE}
+              />
+          }
+
+          <TouchableOpacity>
+            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+              Change photo
+            </Text>
+          </TouchableOpacity>
+
+          <Input 
+            bg="gray.600" 
+            placeholder='Name' 
+          />
+
+          <Input 
+            bg="gray.600" 
+            placeholder="E-mail"
+            isDisabled
+          />
+        </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start" mt={12}>
+            Change password
+          </Heading>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Old password"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="New password"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Confirm new password"
+            secureTextEntry
+          />
+
+          <Button title="Update" mt={4} />
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
