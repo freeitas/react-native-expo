1ae2c51316cc07e438fd43333f17378410ab6f13
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-11-15 13:50:54 -0300

    fix: message error

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -39,7 +39,7 @@ export function Profile() {
 
         if(photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
           return toast.show({
-            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            title: 'This image its too big. Must be less than 5MB.',
             placement: 'top',
             bgColor: 'red.500'
           }) 
